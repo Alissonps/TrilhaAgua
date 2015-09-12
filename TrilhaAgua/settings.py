e1b3b015fa452e6b5244cdfa046518470db30ca5
Alissonps <alissonnpereira@gmail.com>
2015-09-12 11:04:02 -0300

    Modificação do SGBD para o postgre.

--- TrilhaAgua/settings.py
+++ TrilhaAgua/settings.py
@@ -69,8 +69,14 @@ WSGI_APPLICATION = 'TrilhaAgua.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Rede',                      
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
